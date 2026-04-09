5ed4d3fd701149ff39783dd012403273b944b800
oussaaama <elouahidi.oussama.studente@itispaleocapa.it>
2026-04-09 11:28:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,8 @@ async function fetchProduct(barcode) {
   showMessage("Caricamento...");
   productCard.classList.add("hidden");
 
-  const url = `https://world.openfoodfacts.org/api/v0/product/${barcode}.json`;
+  const url = `https://world.openfoodfacts.org/api/v0/product/${barcode}.json?lc=it`;
+
 
   try {
     const response = await fetch(url);
